963a44f45f9e27022448b55145a52662f953a5b2
zhang <2177393158@qq.com>
2022-06-28 17:19:11 +0800

    home 首页开发

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
 
-// #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+
+// 按需导入 $http 对象
+import { $http } from '@escook/request-miniprogram'
+
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+
+// 配置请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求拦截器
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中...'
+	})
+}
+
+// 响应拦截器
+$http.afterRequest = function() {
+	uni.hideLoading()
+}
+
+// 封装的展示消息提示的方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
 
 Vue.config.productionTip = false
 
@@ -11,15 +40,3 @@ const app = new Vue({
     ...App
 })
 app.$mount()
-// #endif
-
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-import App from './App.vue'
-export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
-}
-// #endif
